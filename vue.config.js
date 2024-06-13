efe45f0c2cb9402c36768afd8c9f851e34e4602b
Oscar Zeballos <oscarzeballoscarvallo@gmail.com>
2024-06-13 14:23:22 -0400

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -10,9 +10,24 @@
 // })
 
 module.exports = {
-  transpileDependencies: true,
+  // No se generarán mapas de origen para archivos de producción para reducir el tamaño
+  productionSourceMap: false,
+
+  // Configuración de Webpack para producción
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      // Modo producción
+      config.optimization.splitChunks = {
+        chunks: 'all',
+      };
+      config.optimization.runtimeChunk = 'single';
+    }
+  },
+
+  // Configuración del servidor de desarrollo en vue-cli-service
   devServer: {
-    port: 5000
+    port: 5000,  // El puerto en el que quieres que funcione tu servidor de desarrollo
+    open: true,  // Abre el navegador automáticamente
   }
-}
+};
 
